ff22d3eb70bbc0d35d04f49548af0157039099aa
r_komiya <r_komiya@192.168.227.134>
2024-07-22 10:30:55 +0900

    a

---
+++ src/subject/SubjectService.java
@@ -0,0 +1,10 @@
+package subject;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SubjectService {
+    public List<String> getAllSubjectCodes() {
+        return Arrays.asList("ABC", "DEF", "GHI");
+    }
+}
